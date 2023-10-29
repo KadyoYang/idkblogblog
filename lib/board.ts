98df6ce7873e5c399c936707b888f367775236c2
양찬규 <mrriey00@naver.com>
2023-10-29 20:25:22 +0900

    [feature] getBoard함수 추가

--- lib/board.ts
+++ lib/board.ts
@@ -22,26 +22,31 @@ type Post = {
 };
 
 type Board = {
+  // #TODO board descripton.md 에서 추출하도록 추가
+  name: string;
   content?: string; // board description
   posts: Array<Post>;
   childBoard: Array<Board>;
 };
 
-export const boardBasePath = join(process.cwd(), "_posts", "board");
+export const _POST_BASE_PATH = join(process.cwd(), "_posts");
 
 // #TODO unit test 로 검증 필요
-export function getBoard(basePath: string = boardBasePath): Board {
-  const subDirs = fs.readdirSync(boardBasePath, { withFileTypes: true });
+export function getBoard(dirName: string, path = _POST_BASE_PATH): Board {
+  const basePath = join(path, dirName);
+  const subDirs = fs.readdirSync(basePath, { withFileTypes: true });
 
   const dirs = subDirs.filter((v) => v.isDirectory());
   const mdxFiles = subDirs.filter((v) => v.isFile());
 
+  // console.log(mdxFiles);
   return {
+    name: dirName,
     posts: mdxFiles.map((v) => {
-      const fileContents = fs.readFileSync(v.path, "utf8");
+      const fileContents = fs.readFileSync(join(basePath, v.name), "utf8");
       const { data, content } = matter(fileContents);
       return { content, meta: data };
     }),
-    childBoard: dirs.map((v) => getBoard(v.path)),
+    childBoard: dirs.map((v) => getBoard(v.name, basePath)),
   };
 }
